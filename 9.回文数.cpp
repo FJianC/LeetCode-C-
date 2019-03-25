68c8a5ac87c5959311f180e8dfc15c32a8181ebc
FJianC <fjianc@gmail.com>
2019-03-25 20:46:37 +0800

    continue

---
+++ 9.回文数.cpp
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode.cn id=9 lang=cpp
+ *
+ * [9] 回文数
+ *
+ * https://leetcode-cn.com/problems/palindrome-number/description/
+ *
+ * algorithms
+ * Easy (56.06%)
+ * Total Accepted:    86.9K
+ * Total Submissions: 154.9K
+ * Testcase Example:  '121'
+ *
+ * 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ *
+ * 示例 1:
+ *
+ * 输入: 121
+ * 输出: true
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: -121
+ * 输出: false
+ * 解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+ *
+ *
+ * 示例 3:
+ *
+ * 输入: 10
+ * 输出: false
+ * 解释: 从右向左读, 为 01 。因此它不是一个回文数。
+ *
+ *
+ * 进阶:
+ *
+ * 你能不将整数转为字符串来解决这个问题吗？
+ *
+ */
+#include <vector>
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        // if(x < 0) return false;
+        // std::vector<int> v;
+        // while(x != 0){
+        //     v.push_back(x % 10);
+        //     x /= 10;
+        // }
+        // int i = 0;
+        // int j = v.size() - 1;
+        // while(i < j){
+        //     if (v.at(i++) != v.at(j--)) return false;
+        // }
+        // return true;
+        if(x<0|| (x!=0 &&x%10==0)) return false;
+        int sum=0;
+        while(x>sum)
+        {
+            sum = sum*10+x%10;
+            x = x/10;
+        }
+        return (x==sum)||(x==sum/10);
+    }
+};
