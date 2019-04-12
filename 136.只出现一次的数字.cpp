9f5599ca17f9c3356cee9e170aed41719df2c06d
FJianC <fjianc@gmail.com>
2019-04-12 10:14:10 +0800

    continue

--- 136.只出现一次的数字.cpp
+++ 136.只出现一次的数字.cpp
@@ -32,6 +32,7 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+        /* unordered_map.find--->remove */
         // for(int i = 0; i < nums.size(); i++) {
         //     int j = nums.size() - 1;
         //     for(; j >= 0; j--) {
